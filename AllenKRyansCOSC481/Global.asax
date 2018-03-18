55fa52d992a044d3ea0025d1071b486a91909e45
Azzam <Azzam@DESKTOP-FOB5KG9>
2018-03-18 19:10:34 -0400

    Add project files.

---
+++ AllenKRyansCOSC481/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AllenKRyansCOSC481.MvcApplication" Language="C#" %>
---
+++ AllenKRyansCOSC481/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AllenKRyansCOSC481.Startup))]
+namespace AllenKRyansCOSC481
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
