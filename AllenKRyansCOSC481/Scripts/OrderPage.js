fccff149471b84baba7b0274e9e18f418ac83b0a
mbolin1 <mbolin1@emich.edu>
2018-03-23 19:31:09 -0400

    sending js and css stuff

---
+++ AllenKRyansCOSC481/Scripts/OrderPage.js
@@ -0,0 +1,15 @@
+﻿function visibility(i) {
+	
+	var itemGrp = "itemGroup";
+	var iitemGrp = document.getElementById("ocount").value;
+
+	
+
+	for (var j = 1; j < iitemGrp; j++) {
+		//alert(iitemGrp + j);
+		document.getElementById(itemGrp + j).style.display = "none";
+		//alert("j: "+itemGrp + j);
+	}
+	//alert("i: "+itemGrp + i);
+	document.getElementById("" + itemGrp + i).style.display = "inline";
+}
