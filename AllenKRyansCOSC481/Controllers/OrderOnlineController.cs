c7bc3c4da761e0dde8e5bc9f7c5982fbef1dbb8e
bolinmt <bolinmt@gmail.com>
2018-04-01 20:20:25 -0400

    Refactoring of CartItem both order page and cart fixed to work together and controller handles Post requests now

--- AllenKRyansCOSC481/Controllers/OrderOnlineController.cs
+++ AllenKRyansCOSC481/Controllers/OrderOnlineController.cs
@@ -12,8 +12,45 @@ namespace AllenKRyansCOSC481.Controllers
     {
         private RestaurantContext db = new RestaurantContext();
 
-        public ActionResult AddToCart(int num)
+        [HttpPost]
+        public ActionResult Remove()
         {
+            int amt = 1;
+            int index = 0;
+            int.TryParse(Request["index"], out index);
+            int.TryParse(Request["amount"], out amt);
+
+            List<CartItem> cart = (List<CartItem>)(Session["cart"]);
+
+            for (int i = 0; i < cart.Count; i++)
+            {
+                if (cart[i].index == index)
+                {
+                    cart[i].count -= amt;
+                    if (cart[i].count == 0)
+                    {
+                        cart.Remove(cart[i]);
+                    }
+                    if (cart.Count == 0)
+                    {
+                        Session["Cart"] = null;
+                        break;
+                    }
+                    Session["cart"] = cart;
+
+                    break;
+                }
+            }
+            return RedirectToAction("Cart");
+        }
+
+        [HttpPost]
+        public ActionResult AddToCart()
+        {
+            int cnt = 1;
+            int num = -1;
+            int.TryParse(Request["num"], out num);
+            int.TryParse(Request["Quantity" + num], out cnt);
             List<Item> menu = new List<Item>();
             IEnumerable<Item> Model = db.Items.OrderBy(x => x.Type);
             foreach (var item in Model)
@@ -22,14 +59,17 @@ namespace AllenKRyansCOSC481.Controllers
             }
             if (Session["cart"] == null)
             {
-                Session["cart"] = new List<CartItem>();
-                var lst = (List<CartItem>)(Session["cart"]);
-                lst.Add(new CartItem { item = menu[num], count = Request[]  });
+                var lst = new List<CartItem>();
+                lst.Add(new CartItem { item = menu[num], count = cnt, index = num });
+                lst[lst.Count - 1].calculatePrice();
+                Session["cart"] = lst;
             }
             else
             {
-                var lst = (List<Item>)(Session["cart"]);
-                lst.Add(menu[num]);
+                var lst = (List<CartItem>)(Session["cart"]);
+                lst.Add(new CartItem { count = cnt, item = menu[num], index = num });
+                lst[lst.Count - 1].calculatePrice();
+                Session["cart"] = lst;
             }
             return RedirectToAction("Cart");
         }
@@ -45,10 +85,15 @@ namespace AllenKRyansCOSC481.Controllers
             ViewBag.Message = "Your Online Order";
             List<Item> menu = new List<Item>();
             IEnumerable<Item> Model = db.Items.OrderBy(x => x.Type);
-            foreach(var item in Model)
+            foreach (var item in Model)
             {
                 menu.Add(item);
             }
+            if (Session["cart"] == null)
+            {
+                Session["cart"] = new List<CartItem>();
+            }
+            Session["count"] = 1;
 
             return View(menu);
         }
--- AllenKRyansCOSC481/Models/CartItem.cs
+++ AllenKRyansCOSC481/Models/CartItem.cs
@@ -8,6 +8,12 @@ namespace AllenKRyansCOSC481.Models
     public class CartItem
     {
         public Item item { get; set; }
+        public int index { get; set; } = -1;
         public int count { get; set; } = 1;
+        public double price { get; set; } = 0;
+        public void calculatePrice()
+        {
+            price = item.Price * count;
+        }
     }
 }
